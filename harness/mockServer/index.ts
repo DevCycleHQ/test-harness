3e5844f64f3224f3966eb20388b888f7f2cce299
Adam Wootton <adam@taplytics.com>
2022-11-17 16:54:07 -0500

    chore: set up jest environment mock server connection (#9)
    * chore: set up jest environment mock server connection
    
    * send correct method and headers
    
    * add matcher for header
    
    * fix body passing

---
+++ harness/mockServer/index.ts
@@ -0,0 +1,29 @@
+import Koa from 'koa'
+import Router from '@koa/router'
+import nock from 'nock'
+import axios from 'axios'
+import bodyParser from 'koa-bodyparser'
+
+const scope = nock('https://nock.com')
+export const getServerScope = () => scope
+
+export const initialize = () => {
+    const app = new Koa()
+    const router = new Router()
+
+    router.all('/(.*)', async (ctx) => {
+        const { headers, request } = ctx
+        const response = await axios[request.method.toLowerCase()](`https://nock.com${ctx.request.path}`, request.body, {
+            headers
+        })
+        ctx.body = response.data
+        ctx.status = response.status
+    })
+
+    app
+        .use(bodyParser())
+        .use(router.routes())
+        .use(router.allowedMethods())
+
+    return app.listen(0)
+}
---
+++ harness/nock.test.ts
@@ -0,0 +1,35 @@
+import { getServerScope } from './mockServer'
+import nock from 'nock'
+
+const scope = getServerScope()
+
+describe('nock', () => {
+   it('intercepts requests', async () => {
+        const clientId = '123'
+
+        scope.put(`/client/${clientId}`, { yo: /.*/}).matchHeader('Content-Type', 'application/json').times(2).reply(200, {
+            goats: 'Never in a million years'
+        })
+
+         const result = await fetch(`http://localhost:${global.__MOCK_SERVER_PORT__}/client/${clientId}`, {
+            method: 'put',
+            body: JSON.stringify({
+                yo: 's'
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+
+        const result2 = await fetch(`http://localhost:${global.__MOCK_SERVER_PORT__}/client/${clientId}`, {
+            method: 'put',
+            body: JSON.stringify({
+                yo: 'a'
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+
+        console.log(await result.json())
+        console.log(await result2.json())
+
+        expect(scope.isDone()).toBeTruthy()
+    })
+})
