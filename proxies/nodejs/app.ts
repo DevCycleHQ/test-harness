f1c9214a2f3109e645205d5c9a2d67cb445fabb5
Emma Willis <ehenriks@uwo.ca>
2022-11-17 19:08:47 -0500

    Add /spec endpoint

--- proxies/nodejs/app.ts
+++ proxies/nodejs/app.ts
@@ -1,14 +1,24 @@
 import { initialize } from '@devcycle/nodejs-server-sdk'
 import Koa from 'koa'
+import Router from 'koa-router'
+
 
 async function start() {
     await initialize('<TOKEN>').onClientInitialized()
 
     const app = new Koa()
 
-    app.use(async (ctx) => {
-      ctx.body = 'Hello World'
+    var router = Router()
+    router.get('/spec', (ctx) => {
+      ctx.status = 200
+      ctx.body = {
+        name: "NodeJS",
+        version: "", // TODO add branch name or sdk version here
+        capabilities: ["EdgeDB", "LocalBucketing"],
+      }
     })
+    
+    app.use(router.routes()).use(router.allowedMethods())
 
     // Server!
     console.log('Server started on port 3000')
