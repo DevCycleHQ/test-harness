ecbb9fd02b9d3160f6f5bb8759017e5401c44968
Elliot Campbell <elliotgithub@gmail.com>
2022-11-18 15:15:31 -0500

    add shared types file

---
+++ types/capabilities.ts
@@ -0,0 +1,6 @@
+export enum Capabilities {
+    edgeDB = "EdgeDB",
+    local = "LocalBucketing",
+    cloud = "CloudBucketing",
+    sse = "SSE"
+}
---
+++ types/sdks.ts
@@ -0,0 +1,4 @@
+export enum Sdks {
+    nodejs = "NodeJS",
+
+}
