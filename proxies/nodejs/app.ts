36cfc7614dd8f26685eae9db47f4a6c926cb1668
Emma Willis <ehenriks@uwo.ca>
2022-11-18 14:41:53 -0500

    Add client endpoint to proxy

--- proxies/nodejs/app.ts
+++ proxies/nodejs/app.ts
@@ -1,14 +1,37 @@
-import { initialize } from '@devcycle/nodejs-server-sdk'
+import { DVCClient } from '@devcycle/nodejs-server-sdk'
 import Koa from 'koa'
 import Router from 'koa-router'
+import KoaBody  from 'koa-body'
 
+const data = {
+  clients: {},
+  users: {},
+  commandResults: {}
+}
+
+const handleClient = async (ctx) => {
+  const body = ctx.request.body
+  if (body.clientId === undefined) {
+    ctx.status = 400 
+    ctx.body = "Invalid request: Missing clientId"
+  } else if (!body.sdkKey) {
+    ctx.status = 400 
+    ctx.body = "Invalid request: Missing sdkKey"
+  } else {
+    const client = new DVCClient(ctx.request.body.sdkKey, ctx.request.body.options)
+    data.clients[ctx.request.body.clientId] = client
+    await client.onClientInitialized()
+
+    ctx.status = 200
+  }
+}
 
 async function start() {
-    await initialize('<TOKEN>').onClientInitialized()
-
     const app = new Koa()
+    app.use(KoaBody())
 
     var router = Router()
+
     router.get('/spec', (ctx) => {
       ctx.status = 200
       ctx.body = {
@@ -18,6 +41,8 @@ async function start() {
       }
     })
     
+    router.post('/client', handleClient)
+
     app.use(router.routes()).use(router.allowedMethods())
 
     // Server!
@@ -26,5 +51,3 @@ async function start() {
 }
 
 start()
-
-
