f878df332613c27342ff68db97e8d381c343c36b
Elliot Campbell <elliotgithub@gmail.com>
2022-11-21 11:28:20 -0500

    wip

--- harness/helpers/index.ts
+++ harness/helpers/index.ts
@@ -1,5 +1,6 @@
+import { Sdks } from '../../types/sdks'
 export const forEachSDK = (name, tests) => {
     // get the list of SDK's and their capabilities
-    const SDKs = JSON.parse(process.env.SDKS_TO_TEST) || []
+    const SDKs = JSON.parse(process.env.SDKS_TO_TEST) || Object.values(Sdks)
     describe.each(SDKs)(name, tests)
 }
