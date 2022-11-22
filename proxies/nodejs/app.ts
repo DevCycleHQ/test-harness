b5eab4c739b450d003ef5f693ac3bba2a816e6b1
Parth Suthar <parth.suthar@taplytics.com>
2022-11-22 10:00:01 -0500

    reference correct entity type

--- proxies/nodejs/app.ts
+++ proxies/nodejs/app.ts
@@ -1,10 +1,9 @@
-import { DVCClient, DVCUser } from '@devcycle/nodejs-server-sdk'
-import Koa from 'koa'
-import Router from 'koa-router'
-import bodyParser from 'koa-bodyparser'
-import { handleUser } from './handlers/user'
-import { handleClient } from './handlers/client'
-import { handleLocation } from './handlers/location'
+import { DVCClient, DVCUser } from "@devcycle/nodejs-server-sdk";
+import Koa from "koa";
+import Router from "koa-router";
+import bodyParser from "koa-bodyparser";
+import { handleUser } from "./handlers/user";
+import { handleClient } from "./handlers/client";
 
 type Data = {
   clients: { [key: string]: DVCClient };
@@ -13,38 +12,38 @@ type Data = {
 };
 
 const data: Data = {
-    clients: {},
-    users: {},
-    commandResults: {},
-}
+  clients: {},
+  users: {},
+  commandResults: {},
+};
 
 async function start() {
-    const app = new Koa()
-    app.use(bodyParser())
+  const app = new Koa();
+  app.use(bodyParser());
 
-    const router = new Router()
+  const router = new Router();
 
-    router.get('/spec', (ctx) => {
-        ctx.status = 200
-        ctx.body = {
-            name: 'NodeJS',
-            version: '', // TODO add branch name or sdk version here
-            capabilities: ['EdgeDB', 'LocalBucketing'],
-        }
-    })
+  router.get("/spec", (ctx) => {
+    ctx.status = 200;
+    ctx.body = {
+      name: "NodeJS",
+      version: "", // TODO add branch name or sdk version here
+      capabilities: ["EdgeDB", "LocalBucketing"],
+    };
+  });
 
-    router.post('/client', (ctx: Koa.ParameterizedContext) => {
-        handleClient(ctx, data.clients)
-    })
-    router.post('/user', (ctx: Koa.ParameterizedContext) => {
-        handleUser(ctx, data.users)
-    })
+  router.post("/client", (ctx: Koa.ParameterizedContext) => {
+    handleClient(ctx, data.clients);
+  });
+  router.post("/user", (ctx: Koa.ParameterizedContext) => {
+    handleUser(ctx, data.users);
+  });
 
-    app.use(router.routes()).use(router.allowedMethods())
+  app.use(router.routes()).use(router.allowedMethods());
 
-    // Server!
-    console.log('Server started on port 3000')
-    app.listen(3000)
+  // Server!
+  console.log("Server started on port 3000");
+  app.listen(3000);
 }
 
-start()
+start();
