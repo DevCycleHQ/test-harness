24f407614d72f4dca28ba0c6f94e1d7b05154e00
Parth Suthar <parth.suthar@taplytics.com>
2022-11-28 14:47:47 -0500

    [DVC - 5171] handle callbacks and test with dummy server (#26)
    * reference correct entity type
    
    * add :location call and callback capability
    
    * handle callbacks and test with dummy server
    
    * update call back with the entityType
    
    * fix params checking
    
    * add error handling for callback functions
    
    * fix callback data to be dynamic type
    
    * fix callback calling

--- proxies/nodejs/handlers/location.ts
+++ proxies/nodejs/handlers/location.ts
@@ -1,6 +1,7 @@
 import { DVCClient, DVCUser, DVCVariable } from '@devcycle/nodejs-server-sdk'
 import Koa from 'koa'
 import { getEntityFromType, Data } from '../entityTypes'
+import axios from 'axios'
 
 //HTTP request comes in as string
 type LocationRequestBody = {
@@ -9,52 +10,75 @@ type LocationRequestBody = {
     isAsync: boolean
 }
 
-type parsedParams = (string | boolean | number | object)[];
+type ParsedParams = (string | boolean | number | object | URL)[];
+
+const SUPPORTED_COMMANDS = ['onClientInitialized']
 
 export const handleLocation = async (
     ctx: Koa.ParameterizedContext,
     data: Data,
     body: LocationRequestBody,
-    entity: DVCClient | DVCUser | DVCVariable | any
+    entity: DVCClient | DVCUser | DVCVariable
 ) => {
-
     try {
         const command = body.command
-        const params: parsedParams = parseParams(
+        const params: ParsedParams = parseParams(
             JSON.parse(body.params),
             data
         )
+        const lastParam = params[params.length - 1]
 
-        // TODO: handle async commands
-        // TODO: handle callback before invoking command
-        const resultData = await invokeCommand(
-            entity,
-            command,
-            params,
-            body.isAsync
-        )
+        if (lastParam instanceof URL) {
+            const callbackURL: URL = lastParam
+            if (!SUPPORTED_COMMANDS.includes(command)) {
+                ctx.status = 404
+                ctx.body = {
+                    errorCode: 404,
+                    exception: 'Invalid request: unsupported command',
+                }
+                return ctx
+            }
+            const onCallback = (command) => {
+                axios
+                    .post(callbackURL.href, {
+                        message: `${command} was invoked on ${ctx.request.url}`,
+                    })
+                    .then((resp: any) => console.log('onCallback data ', resp.data)) // just to test the callback
+                    .catch((e) => console.error(e))
+            }
+            entity[command](() => onCallback(command)).catch((e) => console.error(e))
+            ctx.status = 200
+            ctx.body = {
+                message: `${command} attached to ${ctx.request.url}`,
+            }
+        } else {
+            const resultData = await invokeCommand(
+                entity,
+                command,
+                params,
+                body.isAsync
+            )
 
-        const entityType = getEntityFromType(resultData.constructor.name)
+            const entityType = getEntityFromType(resultData.constructor.name)
 
-        const commandId = data.commandResults[entityType.toLowerCase()] !== undefined ?
-            Object.keys(data.commandResults[entityType.toLowerCase()]).length :
-            0
+            const commandId = data.commandResults[entityType.toLowerCase()] !== undefined ?
+                Object.keys(data.commandResults[entityType.toLowerCase()]).length :
+                0
 
-        if (data.commandResults[entityType.toLowerCase()] === undefined) {
-            data.commandResults[entityType.toLowerCase()] = {}
-        }
-        data.commandResults[entityType.toLowerCase()][commandId] = resultData
+            if (data.commandResults[entityType.toLowerCase()] === undefined) {
+                data.commandResults[entityType.toLowerCase()] = {}
+            }
+            data.commandResults[entityType.toLowerCase()][commandId] = resultData
 
-        ctx.status = 200
-        ctx.set('Location', `command/${entityType.toLowerCase()}/${commandId}`)
-        ctx.body = {
-            entityType: entityType,
-            data: resultData,
-            logs: [], // TODO add logs here
+            ctx.status = 200
+            ctx.set('Location', `command/${entityType.toLowerCase()}/${commandId}`)
+            ctx.body = {
+                entityType: entityType,
+                data: resultData,
+                logs: [], // TODO add logs here
+            }
         }
         console.log('dataObject: ', data)
-        console.log('logger: ', entity.logger.info)
-
     } catch (error) {
         console.error(error)
         if (body.isAsync) {
@@ -69,7 +93,6 @@ export const handleLocation = async (
                 errorCode: error.code,
                 exception: error.message,
             }
-
         }
     }
 }
@@ -100,6 +123,7 @@ const getEntityFromLocation = (location: string, data: Data) => {
         const id = urlParts[urlParts.length - 1]
         console.log('command: ', command)
         if (command === 'command') {
+            // debug logs
             console.log('data: ', data)
             console.log('commandResults: ', data.commandResults)
             console.log('entityType: ', data.commandResults[entityType])
@@ -110,13 +134,15 @@ const getEntityFromLocation = (location: string, data: Data) => {
     return undefined
 }
 
-const parseParams = (params: object | any, data: Data): parsedParams => {
-    const parsedParams: parsedParams = []
+const parseParams = (params: object | any, data: Data): ParsedParams => {
+    const parsedParams: ParsedParams = []
     params.forEach((element) => {
         if (element.value !== undefined) {
             parsedParams.push(element.value)
         } else if (element.location !== undefined) {
             parsedParams.push(getEntityFromLocation(element.location, data))
+        } else if (element.callbackURL !== undefined) {
+            parsedParams.push(new URL(element.callbackURL))
         }
     })
     return parsedParams
@@ -125,17 +151,13 @@ const parseParams = (params: object | any, data: Data): parsedParams => {
 const invokeCommand = async (
     entity: DVCClient | DVCUser | DVCVariable | any,
     command: string,
-    params: parsedParams,
+    params: ParsedParams,
     isAsync: boolean) => {
     if (isAsync) {
         const result = await entity[command](...params)
         return result
     }
-    console.log('entity: ', entity)
-    console.log('command: ', command)
-    console.log('fn: ', entity[command])
     return entity[command](...params)
-
 }
 
 export const validateLocationRequest =
@@ -144,17 +166,17 @@ export const validateLocationRequest =
         const body = ctx.request.body as LocationRequestBody
 
         if (entity === undefined) {
-            ctx.status = 400
+            ctx.status = 404
             ctx.body = {
-                errorCode: 400,
+                errorCode: 404,
                 errorMessage: 'Invalid request: missing entity',
             }
             return ctx
         }
         if (body.command === undefined) {
-            ctx.status = 400
+            ctx.status = 404
             ctx.body = {
-                errorCode: 400,
+                errorCode: 404,
                 errorMessage: 'Invalid request: missing command',
             }
             return ctx
