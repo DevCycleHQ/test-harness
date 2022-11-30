5b50c23e99a4bb097dcfe1f23c0bb8dd4a5a3360
Elliot Campbell <elliotgithub@gmail.com>
2022-11-30 11:51:53 -0500

    added some more mock data for features/variables

---
+++ harness/mockData/features.ts
@@ -0,0 +1,18 @@
+export const features = {
+    "test-harness": {
+        "_id": "638680d6fcb67b96878d90e6",
+        "key": "test-harness",
+        "type": "release",
+        "_variation": "638680d6fcb67b96878d90ec",
+        "variationName": "Variation On",
+        "variationKey": "variation-on"
+    },
+    "schedule-feature": {
+        "_id": "6386813a59f1b81cc9e6c68d",
+        "key": "schedule-feature",
+        "type": "release",
+        "_variation": "6386813a59f1b81cc9e6c693",
+        "variationName": "Variation On",
+        "variationKey": "variation-on"
+    }
+}
