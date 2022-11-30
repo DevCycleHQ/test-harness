cf45411a40498dfb684e200849f8b42bc8815176
elliotCamblor <elliot@taplytics.com>
2022-11-30 11:54:14 -0500

    added some more mock data for features/variables (#30)

---
+++ harness/mockData/variables.ts
@@ -0,0 +1,34 @@
+export const variables = {
+    "bool-var": {
+        "_id": "638681f059f1b81cc9e6c7fa",
+        "key": "bool-var",
+        "type": "Boolean",
+        "value": true
+    },
+    "string-var": {
+        "_id": "638681f059f1b81cc9e6c7fb",
+        "key": "string-var",
+        "type": "String",
+        "value": "string"
+    },
+    "number-var": {
+        "_id": "638681f059f1b81cc9e6c7fc",
+        "key": "number-var",
+        "type": "Number",
+        "value": 1
+    },
+    "json-var": {
+        "_id": "638681f059f1b81cc9e6c7fd",
+        "key": "json-var",
+        "type": "JSON",
+        "value": {
+            "facts": true
+        }
+    },
+    "schedule-feature": {
+        "_id": "6386813a59f1b81cc9e6c68f",
+        "key": "schedule-feature",
+        "type": "Boolean",
+        "value": true
+    }
+}
