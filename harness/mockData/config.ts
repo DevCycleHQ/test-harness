5a0e9d0fc5b1ab812fd309b446b84a7e4a657dcc
Emma Willis <ehenriks@uwo.ca>
2022-12-02 18:09:53 -0500

    Add local and cloud allFeatures tests

--- harness/mockData/config.ts
+++ harness/mockData/config.ts
@@ -181,3 +181,23 @@ export const config: ConfigBody = {
         "apiKey": "fakekey"
     }
 }
+
+export const expectedFeaturesVariationOn = {
+    'test-harness': {
+        _id: '638680d6fcb67b96878d90e6',
+        type: 'release',
+        key: 'test-harness',
+        _variation: '638680d6fcb67b96878d90ec',
+        variationName: 'Variation On',
+        variationKey: 'variation-on'
+    },
+    'schedule-feature': {
+        _id: '6386813a59f1b81cc9e6c68d',
+        type: 'release',
+        key: 'schedule-feature',
+        _variation: '6386813a59f1b81cc9e6c693',
+        variationName: 'Variation On',
+        variationKey: 'variation-on'
+    }
+}
+
