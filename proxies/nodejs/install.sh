5d8569c544c1756d69fdb85446d9c06706400b37
Jason Salaber <jcsalaber@hotmail.com>
2022-12-05 17:30:48 -0500

    feat: checkout sha of node js sdk if given from workflow

--- proxies/nodejs/install.sh
+++ proxies/nodejs/install.sh
@@ -1,4 +1,12 @@
-if [ -z "$NODEJS_SDK_VERSION" ]; then
+if [ "GITHUB_SHA" ]; then
+    git clone https://github.com/DevCycleHQ/js-sdks.git
+    cd js-sdks
+    git checkout $GITHUB_SHA
+    yarn
+    NX_DAEMON=false yarn nx build nodejs --verbose
+    cd ..
+    yarn add file:js-sdks/dist/sdk/nodejs/
+elif [ -z "$NODEJS_SDK_VERSION" ]; then
     yarn add @devcycle/nodejs-server-sdk
 else
     yarn add @devcycle/nodejs-server-sdk@$NODEJS_SDK_VERSION
