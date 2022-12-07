dd9645b4e784610caad543520b20910628d97bb2
Jason Salaber <jcsalaber@hotmail.com>
2022-12-07 13:24:22 -0500

    feat: checkout sha of node js sdk if given from workflow (#38)
    * feat: checkout sha of node js sdk if given from workflow
    
    * chore: added SDK_TO_TEST variable for docker images
    
    * chore: added dotenv to use for ci step

--- proxies/nodejs/install.sh
+++ proxies/nodejs/install.sh
@@ -1,4 +1,14 @@
-if [ -z "$NODEJS_SDK_VERSION" ]; then
+if [ -n "$GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
+    git clone https://github.com/DevCycleHQ/js-sdks.git
+    cd js-sdks
+    git checkout $GITHUB_SHA
+    yarn
+    NX_DAEMON=false yarn nx build nodejs --verbose
+    cd ..
+    yarn add file:js-sdks/dist/sdk/nodejs/
+    yarn add file:js-sdks/lib/shared/bucketing-assembly-script/
+    yarn add file:js-sdks/lib/shared/types/
+elif [ -z "$NODEJS_SDK_VERSION" ]; then
     yarn add @devcycle/nodejs-server-sdk
 else
     yarn add @devcycle/nodejs-server-sdk@$NODEJS_SDK_VERSION
