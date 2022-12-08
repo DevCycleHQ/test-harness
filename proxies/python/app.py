9ea5b552b12ce5bf608c5afffc9bca5d515a7d7d
Adam Wootton <adam@taplytics.com>
2022-12-08 10:12:35 -0500

    add client endpoint for python (#49)
    * - add client endpoint for python
    
    * fix client typo

--- proxies/python/app.py
+++ proxies/python/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from devcycle_python_sdk import Configuration, DVCOptions, DVCClient, UserData, Event
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -29,3 +30,30 @@ def spec():
         "version": "", # TODO add branch name or SDK version here
         "capabilities": ["EdgeDB", "CloudBucketing"]
     }
+
+@app.post("/client")
+def client():
+    body = request.get_json()
+    client_id, sdk_key, options = [body.get(k, None) for k in ('clientId', 'sdkKey', 'options')]
+
+    if (client_id == None):
+        error = {
+            "error": "Invalid request: missing clientId"
+        }
+
+        return error, 400
+
+    configuration = Configuration()
+    configuration.api_key['Authorization'] = sdk_key
+
+    dvc_client = DVCClient(configuration, options)
+
+    dataStore['clients'][client_id] = dvc_client
+
+    success = {
+        "message": "success"
+    }
+
+    return success, 201, {"Location": 'client/' + client_id}
+
+
