b71f69c2bb888c8b994bc2b2b3a2253bef4573ff
elliotCamblor <elliot@taplytics.com>
2022-12-15 13:53:32 -0500

    add spec endpoint (#68)

---
+++ proxies/java/src/main/java/com/dvc_harness/proxy/controller/Spec.java
@@ -0,0 +1,25 @@
+package com.dvc_harness.proxy.controller;
+
+public class Spec {
+    private final String name;
+    private final String version;
+    private final String[] capabilities;
+
+    public Spec() {
+        name = "java";
+        capabilities = new String[]{"LocalBucketing", "CloudBucketing", "EdgeDB"};
+        version = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getCapabilities() {
+        return capabilities;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
