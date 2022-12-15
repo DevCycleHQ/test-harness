e0a0854155b79eb99332f1f9be975d14b758e147
Adam Wootton <adam@taplytics.com>
2022-12-15 16:02:37 -0500

    add client endpoint for java proxy

---
+++ proxies/java/src/main/java/com/dvc_harness/proxy/models/ClientOptions.java
@@ -0,0 +1,10 @@
+package com.dvc_harness.proxy.models;
+
+public class ClientOptions {
+    public Boolean enableEdgeDB;
+    public String baseURLOverride;
+
+    public Integer eventFlushIntervalMS;
+
+    public Integer configPollingIntervalMS;
+}
---
+++ proxies/java/src/main/java/com/dvc_harness/proxy/models/ClientRequestBody.java
@@ -0,0 +1,8 @@
+package com.dvc_harness.proxy.models;
+
+public class ClientRequestBody {
+    public String clientId;
+    public String sdkKey;
+    public ClientOptions options;
+    public Boolean enableCloudBucketing;
+}
