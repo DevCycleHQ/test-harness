67fa024294a24a0b71cb3d77c639f6b62e816706
Adam Wootton <adam@taplytics.com>
2022-12-22 13:15:18 -0500

    remove dotnet

--- harness/types/sdks.ts
+++ harness/types/sdks.ts
@@ -1,7 +1,7 @@
 export const Sdks = {
     nodejs: "NodeJS",
     // TODO add once Dotnet is ready
-    dotnet: "DotNet",
+    // dotnet: "DotNet",
     // TODO add once python is ready
     // python: "Python"
 }
