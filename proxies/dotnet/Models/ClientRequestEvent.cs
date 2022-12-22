5c745021b230962436a2248b2da2e3f2f1dffc1d
Elliot Campbell <elliotgithub@gmail.com>
2022-12-22 12:09:17 -0500

    wip

---
+++ proxies/dotnet/Models/ClientRequestEvent.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using static DevCycle.SDK.Server.Common.Model.Event;
+
+[DataContract]
+public class ClientRequestEvent {
+    [JsonProperty("type")]
+    public string? Type { get; set; }
+
+    [JsonProperty("target")]
+    public string? Target { get; set; }
+
+    [JsonProperty("value")]
+    public double Value { get; set; }
+
+    [JsonProperty("metaData")]
+    public Dictionary<string, object>? MetaData { get; set; }
+
+    [JsonProperty("date")]
+    public DateTime? Date { get; set; }
+
+    public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class ClientRequestEvent {\n");
+            sb.Append("  Type: ").Append(Type).Append("\n");
+            sb.Append("  Target: ").Append(Target).Append("\n");
+            sb.Append("  Date: ").Append(Date).Append("\n");
+            // sb.Append("  Value: ").Append(Value).Append("\n");
+            sb.Append("  MetaData: ").Append(MetaData).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+}
