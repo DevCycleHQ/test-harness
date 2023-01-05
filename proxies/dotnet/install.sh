2a00339a6307de4331b401a5e98924275b7d053c
Adam Wootton <adam@taplytics.com>
2023-01-05 17:24:28 -0500

    fix checking out the dotnet SDK from github SHA (#119)
    * fix checking out the dotnet SDK from github SHA
    
    * update log
    
    * remove cat

--- proxies/dotnet/install.sh
+++ proxies/dotnet/install.sh
@@ -1,35 +1,35 @@
-if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"dotnet"* ]]; then
+#!/bin/bash
+
+if [[ -n "$SDK_GITHUB_SHA" && "$SDKS_TO_TEST" == *"dotnet"* ]]; then
+    echo "Checking out SDK at $SDK_GITHUB_SHA"
     # changing directory so dotnet doesn't run again when starting up the server
     cd ..
     git clone https://github.com/DevCycleHQ/dotnet-server-sdk.git
     cd dotnet-server-sdk
     git checkout $SDK_GITHUB_SHA
-    cd DevCycle.SDK.Server.Common
-    dotnet build
-    cd ..
-    cd DevCycle.SDK.Server.Local
-    dotnet build
-    cd ..
-    cd DevCycle.SDK.Server.Cloud
-    dotnet build
-    cd ../src
-    dotnet add package DevCycle.SDK.Server.Common -s "../dotnet-server-sdk/DevCycle.SDK.Server.Common/bin/Debug"
+    dotnet restore
+    cd ../../src
+    dotnet remove package DevCycle.SDK.Server.Local
+    dotnet remove package DevCycle.SDK.Server.Cloud
+    dotnet add reference "../dotnet-server-sdk/DevCycle.SDK.Server.Local/DevCycle.SDK.Server.Local.csproj"
+    dotnet add reference "../dotnet-server-sdk/DevCycle.SDK.Server.Cloud/DevCycle.SDK.Server.Cloud.csproj"
+    dotnet restore
+    exit 0
 fi
 
 if [ "$DOTNET_LOCAL_SDK_VERSION" ]; then
     echo "Installing .NET Local SDK Version ${DOTNET_LOCAL_SDK_VERSION}"
     dotnet add package DevCycle.SDK.Server.Local --version $DOTNET_LOCAL_SDK_VERSION
-elif [ -z "$SDK_GITHUB_SHA" ]; then
+else
+    echo "Installing .NET Local SDK Version latest"
     dotnet add package DevCycle.SDK.Server.Local
-else 
-    dotnet add package DevCycle.SDK.Server.Local -s "../dotnet-server-sdk/DevCycle.SDK.Server.Local/bin/Debug"
 fi
 
 if [ "$DOTNET_CLOUD_SDK_VERSION" ]; then
     echo "Installing .NET Cloud SDK Version ${DOTNET_CLOUD_SDK_VERSION}"
     dotnet add package DevCycle.SDK.Server.Cloud --version $DOTNET_CLOUD_SDK_VERSION
-elif [ -z "$SDK_GITHUB_SHA" ]; then
+else
+    echo "Installing .NET Cloud SDK Version latest"
     dotnet add package DevCycle.SDK.Server.Cloud
-else 
-    dotnet add package DevCycle.SDK.Server.Cloud -s "../dotnet-server-sdk/DevCycle.SDK.Server.Cloud/bin/Debug"
 fi
+
