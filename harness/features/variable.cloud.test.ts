36025c3f3942ee85966b8315c5cc770fbf863946
Jason Salaber <jcsalaber@hotmail.com>
2023-01-06 12:30:28 -0500

    chore: added a guide.md to prescribe how to write test harness tests

--- harness/features/variable.cloud.test.ts
+++ harness/features/variable.cloud.test.ts
@@ -1,27 +1,41 @@
 import {
     forEachSDK,
-    describeIf,
     forEachVariableType,
     variablesForTypes,
     CloudTestClient, describeCapability, expectErrorMessageToBe
 } from '../helpers'
-import { Capabilities, SDKCapabilities } from '../types'
+import { Capabilities } from '../types'
 import { getServerScope } from '../nock'
 
+// This is our proxy scope that is used to mock endpoints that are called in the SDK
 const scope = getServerScope()
 
 describe('Variable Tests - Cloud', () => {
+    // This helper method enumerates all the SDKs and calls each test suite
+    // for each SDK, and creates a test client from the name.
+    // All supported SDKs can be found under harness/types/sdks.ts
     forEachSDK((name) => {
         const testClient = new CloudTestClient(name)
 
         beforeAll(async () => {
+            // Creating a client will pass to the proxy server by default: 
+            // - sdk key based on the client id created when creating the client
+            // - urls for the bucketing/config/events endpoints to redirect traffic
+            // into the proxy server so nock can mock out the responses
+            // - options like should wait for initialization, or should 
+            // expect it to error out
             await testClient.createClient({
                 enableCloudBucketing: true
             })
         })
 
+        // This describeCapability only runs if the SDK has the "cloud" capability.
+        // Capabilities are located under harness/types/capabilities and follow the same
+        // name mapping from the sdks.ts file under harness/types/sdks.ts
         describeCapability(name, Capabilities.cloud)(name, () => {
             it('will throw error variable called with invalid key', async () => {
+                // Helper method calls the proxy server to trigger the "variable" method in
+                // the SDK
                 const variableResponse = await testClient.callVariable(
                     { user_id: 'user1' },
                     null,
@@ -29,6 +43,8 @@ describe('Variable Tests - Cloud', () => {
                     true
                 )
                 const error = await variableResponse.json()
+                // Helper method to equate error messages from the error object returned
+                // from the proxy server
                 expectErrorMessageToBe(error.asyncError, 'Missing parameter: key')
             })
 
@@ -45,6 +61,11 @@ describe('Variable Tests - Cloud', () => {
             })
 
             it('should call variables API without edgeDB option', async () => {
+                // This allows us to mock out the our specific client (using the clientId),
+                // allowing us to have multiple mock APIs serving different clients without
+                // conflicting. We can match on specific criteria, like headers and the query params
+                // to specify which call we want to mock, and reply with a status code and an object
+                // as a response
                 scope
                     .post(`/client/${testClient.clientId}/v1/variables/var_key`, (body) => body.user_id === 'user1')
                     .matchHeader('Content-Type', 'application/json')
@@ -70,6 +91,8 @@ describe('Variable Tests - Cloud', () => {
             it('should call variables API with edgeDB option', async () => {
                 const testClient = new CloudTestClient(name)
 
+                // Some tests require extra params for startup, so we can just create a new test client
+                // for these specific tests.
                 await testClient.createClient({
                     enableCloudBucketing: true,
                     enableEdgeDB: true
@@ -95,7 +118,6 @@ describe('Variable Tests - Cloud', () => {
                     'default_value'
                 )
                 await variableResponse.json()
-
             })
 
             it('should return default if mock server\
@@ -118,6 +140,9 @@ describe('Variable Tests - Cloud', () => {
                 )
                 const variable = await variableResponse.json()
 
+                // We can expect that the object we mocked out earlier is going to be 
+                // what is returned to us from the proxy server and verify the entityType
+                // is of type "Variable"
                 expect(variable).toEqual(expect.objectContaining({
                     entityType: 'Variable',
                     data: {
@@ -130,6 +155,8 @@ describe('Variable Tests - Cloud', () => {
                 }))
             })
 
+            // Instead of writing tests for each different type we support (String, Boolean, Number, JSON), 
+            // this function enumerates each type and runs all tests encapsulated within it to condense repeated tests.
             forEachVariableType((type) => {
                 it(`should return default ${type} variable if mock server returns undefined`, async () => {
                     scope
