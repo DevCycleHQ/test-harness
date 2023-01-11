101ae8920512d638ecba79544d689a21dbf65a68
Adam Wootton <adam@taplytics.com>
2023-01-11 12:36:32 -0500

    working SDK version installation

--- proxies/go/install.sh
+++ proxies/go/install.sh
@@ -1,7 +1,7 @@
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"go"* ]]; then
-    go get "github.com/DevCycleHQ/go-server-sdk@$SDK_GITHUB_SHA"
+    go get "github.com/devcyclehq/go-server-sdk@$SDK_GITHUB_SHA"
 elif [ -z "$GO_SDK_VERSION" ]; then
-    go get "github.com/DevCycleHQ/go-server-sdk"
+    go get "github.com/devcyclehq/go-server-sdk"
 else
-    go get "github.com/DevCycleHQ/go-server-sdk@$GO_SDK_VERSION"
+    go get "github.com/devcyclehq/go-server-sdk@$GO_SDK_VERSION"
 fi
