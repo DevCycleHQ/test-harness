e9c17fcc6199014f428312d2915842a4ced079b4
Emma Willis <ehenriks@uwo.ca>
2023-01-12 11:37:46 -0500

    Add datastore to go proxy

--- proxies/go/app.go
+++ proxies/go/app.go
@@ -6,8 +6,24 @@ import (
 	"log"
 	"net/http"
 
+	devcycle "github.com/devcyclehq/go-server-sdk"
 	"github.com/gorilla/mux"
 )
+
+type DataStore = struct {
+	clients        map[string]devcycle.DVCClient
+	commandResults map[string]map[int]any
+}
+
+var datastore = DataStore{
+	make(map[string]devcycle.DVCClient),
+	map[string]map[int]any{
+		"variable":     make(map[int]any),
+		"allVariables": make(map[int]any),
+		"allFeatures":  make(map[int]any),
+		"track":        make(map[int]any),
+	},
+}
 
 func specHandler(w http.ResponseWriter, r *http.Request) {
 	res := struct {
