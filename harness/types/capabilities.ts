4704e69ddf3a3eb1db171cb0d1b29f9d420576f3
Nika Salamadze <33500361+nsalamad@users.noreply.github.com>
2023-01-16 13:51:57 -0500

    Add Go SDK to list of SDKs to test (#142)

--- harness/types/capabilities.ts
+++ harness/types/capabilities.ts
@@ -9,5 +9,6 @@ export const SDKCapabilities = {
     NodeJS: [Capabilities.edgeDB, Capabilities.local, Capabilities.cloud],
     Python: [Capabilities.cloud, Capabilities.edgeDB],
     DotNet: [Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
-    Java: [Capabilities.cloud, Capabilities.local, Capabilities.edgeDB]
+    Java: [Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
+    Go: [Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
 }
