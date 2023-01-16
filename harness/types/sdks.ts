05d1a5f27524e311673da3a68b647ecef04fb496
Nika Salamadze <nika.salamadze@taplytics.com>
2023-01-16 13:47:49 -0500

    Add Go SDK to list of SDKs to test

--- harness/types/sdks.ts
+++ harness/types/sdks.ts
@@ -1,6 +1,7 @@
 export const Sdks = {
-    nodejs: "NodeJS",
-    dotnet: "DotNet"
+    nodejs: 'NodeJS',
+    dotnet: 'DotNet',
     // TODO add once python is ready
     // python: "Python"
+    go: 'Go'
 }
