281d25a1c143ea01b5debcd89cc8da6624683fd4
Nika Salamadze <33500361+nsalamad@users.noreply.github.com>
2023-01-18 11:48:10 -0500

    fix go proxy install.sh (#146)

--- proxies/go/install.sh
+++ proxies/go/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"go"* ]]; then
     go get "github.com/devcyclehq/go-server-sdk@$SDK_GITHUB_SHA"
 elif [ -z "$GO_SDK_VERSION" ]; then
