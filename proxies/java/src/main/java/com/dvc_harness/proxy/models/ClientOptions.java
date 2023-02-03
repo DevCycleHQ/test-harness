9f12df394e8100c35df0090159cb070b10948524
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2023-02-03 10:34:03 -0500

    [fix] update Java Proxy Controller to accept URI Override options. Update ClientOptions model for URI Overrides

--- proxies/java/src/main/java/com/dvc_harness/proxy/models/ClientOptions.java
+++ proxies/java/src/main/java/com/dvc_harness/proxy/models/ClientOptions.java
@@ -2,7 +2,10 @@ package com.dvc_harness.proxy.models;
 
 public class ClientOptions {
     public Boolean enableEdgeDB;
-    public String baseURLOverride;
+    
+    public String bucketingAPIURI;
+    public String eventsAPIURI;
+    public String configCDNURI;
 
     public Integer eventFlushIntervalMS;
 
