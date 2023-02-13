0c43d1a7b25860ca6de16b3168f6fcd5c38efb9c
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2023-02-13 16:37:59 -0500

    [fix] add asyncError field to ExceptionResponse to properly match test expectations

--- proxies/java/src/main/java/com/dvc_harness/proxy/models/ExceptionResponse.java
+++ proxies/java/src/main/java/com/dvc_harness/proxy/models/ExceptionResponse.java
@@ -2,10 +2,12 @@ package com.dvc_harness.proxy.models;
 
 public class ExceptionResponse extends BaseResponse {
     public final String exception;
+    public final String asyncError;
     public StackTraceElement[] stack;
 
     public ExceptionResponse(Exception error) {
         this.exception = error.toString();
+        this.asyncError = error.toString();
         this.stack = error.getStackTrace();
     }
 }
