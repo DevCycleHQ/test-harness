10ced6a724e14ba3f804e028acf63ff6c85db2be
Nika Salamadze <nika.salamadze@taplytics.com>
2023-02-24 11:35:24 -0500

    add /spec endpoint for ruby proxy

--- proxies/ruby/app.rb
+++ proxies/ruby/app.rb
@@ -7,3 +7,11 @@ set :port, 3000
 get '/hello_world' do
   'Hello World'
 end
+
+get '/spec' do
+  {
+    name: 'Ruby',
+    version: '', # TODO: Add branch name or SDK version here
+    capabilities: %w[EdgeDB LocalBucketing]
+  }.to_json
+end
