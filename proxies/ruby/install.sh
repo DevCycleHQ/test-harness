261a24427ba1f60fa88cb6e074a95ea6adb5531d
Jamie Sinn <jamie.sinn@taplytics.com>
2023-02-24 14:55:14 -0500

    Install script

--- proxies/ruby/install.sh
+++ proxies/ruby/install.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Install script thingy."
+if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"go"* ]]; then
+  sed -i "s|gem 'devcycle-ruby-server-sdk'|gem 'devcycle-ruby-server-sdk', github: 'DevCycleHQ/ruby-server-sdk', ref: '$SDK_GITHUB_SHA'|g" Gemfile
+elif [ -z "$RUBY_SDK_VERSION" ]; then
+    exit 0
+else
+    sed -i "s|gem 'devcycle-ruby-server-sdk'|gem 'devcycle-ruby-server-sdk', '$RUBY_SDK_VERSION'|g" Gemfile
+fi
