b903d3b11d292e86c0229433e6b721d987f30134
Jamie Sinn <james.sinn@sinndevelopment.com>
2023-02-27 12:57:07 -0500

    DVC-6336 - Dockerize Ruby (#200)
    * Dockerfile
    
    * Install script

---
+++ proxies/ruby/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"go"* ]]; then
+  sed -i "s|gem 'devcycle-ruby-server-sdk'|gem 'devcycle-ruby-server-sdk', github: 'DevCycleHQ/ruby-server-sdk', ref: '$SDK_GITHUB_SHA'|g" Gemfile
+elif [ -z "$RUBY_SDK_VERSION" ]; then
+    exit 0
+else
+    sed -i "s|gem 'devcycle-ruby-server-sdk'|gem 'devcycle-ruby-server-sdk', '$RUBY_SDK_VERSION'|g" Gemfile
+fi
