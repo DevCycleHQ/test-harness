d9c794f27a56fa2e9b1b86df05ccc7c90b5fb439
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-04-17 08:35:04 -0700

    C# SDK now has setClientCustomData, adding test harness support

--- harness/types/capabilities.ts
+++ harness/types/capabilities.ts
@@ -11,7 +11,7 @@ export const Capabilities = {
 export const SDKCapabilities = {
     NodeJS: [Capabilities.events, Capabilities.edgeDB, Capabilities.local, Capabilities.cloud],
     Python: [Capabilities.events, Capabilities.cloud, Capabilities.edgeDB],
-    DotNet: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
+    DotNet: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB, Capabilities.clientCustomData],
     Java: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
     Go: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB, Capabilities.clientCustomData, Capabilities.multithreading],
     GoNative: [Capabilities.local, Capabilities.clientCustomData, Capabilities.events],
