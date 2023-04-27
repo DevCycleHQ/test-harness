b502e5547e1933ef06ea329efd0b04b039b745c2
Jamie Sinn <james.sinn@sinndevelopment.com>
2023-04-27 13:48:50 -0400

    Enable events for gonative

--- harness/types/capabilities.ts
+++ harness/types/capabilities.ts
@@ -14,6 +14,6 @@ export const SDKCapabilities = {
     DotNet: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
     Java: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB],
     Go: [Capabilities.events, Capabilities.cloud, Capabilities.local, Capabilities.edgeDB, Capabilities.clientCustomData, Capabilities.multithreading],
-    GoNative: [Capabilities.local, Capabilities.clientCustomData],
+    GoNative: [Capabilities.local, Capabilities.clientCustomData, Capabilities.events],
     Ruby: [Capabilities.events, Capabilities.local, Capabilities.clientCustomData]
 }
