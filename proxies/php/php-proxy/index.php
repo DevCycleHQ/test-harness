77b50e46659601c2b48d7a71d73f6f1e61d160a5
Jamie Sinn <jamie.sinn@taplytics.com>
2023-05-10 15:11:26 -0400

    Route handling + php proxy setup

--- proxies/php/php-proxy/index.php
+++ proxies/php/php-proxy/index.php
@@ -1,46 +1,44 @@
 <?php
+
 use DevCycle\Model\UserData;
+use JetBrains\PhpStorm\NoReturn;
+
 require_once(__DIR__ . '/vendor/autoload.php');
 
 $pathArgs = explode('/', getenv('REQUEST_URI'));
 
-// Configure API key authorization
-$config = DevCycle\Configuration::getDefaultConfiguration()
-    ->setApiKey('Authorization', $_ENV["DVC_SERVER_SDK_KEY"]);
-// Uncomment the below lines to use unix domain sockets:
-//->setHost("http:/v1")
-//->setUDSPath("/tmp/phpsock.sock");
-$options = new DevCycle\Model\DVCOptions(false);
-$apiInstance = new DevCycle\Api\DVCClient(
-    $config,
-    dvcOptions: $options
-);
-$user_data = new UserData(array(
-    "user_id" => "test"
-));
-
 switch ($pathArgs[1]) {
     case "spec":
         handleSpec();
-        break;
     case "client":
-        handleClient();
-        break;
+        handleCommand($pathArgs, true);
     case "command":
-        handleCommand();
-        break;
+        handleCommand($pathArgs, false);
+    default:
+        exit(404);
+}
+#[NoReturn] function handleCommand(array $pathArgs, bool $isClient): void
+{
+    if ($_SERVER['REQUEST_METHOD'] !== "POST")
+    {
+        exit(405);
+    }
+
+    $entityBody = json_decode(file_get_contents('php://input'));
+    if (sizeof($pathArgs) < 3 && !$isClient )
+    {
+        exit(400);
+    }
+    echo "Path: $pathArgs[1]\n";
+    echo "IsClient: $isClient\n";
+    $command = $pathArgs[2];
+    echo "Command: $command\n";
+    var_dump($entityBody);
+    exit();
 }
 
-
-function handleClient() {
-
-}
-
-function handleCommand() {
-
-}
-
-function handleSpec() {
+#[NoReturn] function handleSpec(): void
+{
     echo json_encode(["name" => "PHP", "version" => "1.0.0", "capabilities" => ["LocalBucketing", "CloudBucketing", "Events"]]);
-    exit;
+    exit();
 }
