20622157707cc6833a9af90ad349c3e4f58ecab5
Jamie Sinn <jamie.sinn@taplytics.com>
2023-05-11 12:56:54 -0400

    Add PHP to Jest

--- jest.config.ts
+++ jest.config.ts
@@ -59,6 +59,13 @@ const projects = [
             LOCAL_HOST_BINDING: '0.0.0.0'
         }
     },
+    {
+        ...commonConfig,
+        displayName: 'PHP',
+        globals: {
+            JEST_PROJECT_SDK_TO_TEST: 'PHP',
+        }
+    },
     // TODO uncomment once Java is ready
     // {
     //     ...commonConfig,
