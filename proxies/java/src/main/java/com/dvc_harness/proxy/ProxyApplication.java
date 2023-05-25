a6440a1ebd73dcea5d36755c37f2b87643a3db6b
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-05-25 07:24:01 -0700

    Adding waitForInitialization support and some additional logging to the proxy

--- proxies/java/src/main/java/com/dvc_harness/proxy/ProxyApplication.java
+++ proxies/java/src/main/java/com/dvc_harness/proxy/ProxyApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProxyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication app = new SpringApplication(ProxyApplication.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", "3000"));
 		app.run(args);
+		System.out.println("Java Runtime: " + System.getProperty("java.runtime.version") + " (" + System.getProperty("os.arch") + " | " + System.getProperty("sun.arch.data.model") + " bit)");
 	}
 
 }
--- proxies/java/src/main/java/com/dvc_harness/proxy/controller/ProxyController.java
+++ proxies/java/src/main/java/com/dvc_harness/proxy/controller/ProxyController.java
@@ -9,6 +9,7 @@ import com.dvc_harness.proxy.models.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -66,6 +67,21 @@ public class ProxyController {
 
                 DVCLocalClient client = new DVCLocalClient(body.sdkKey, builder.build());
 
+
+                if(body.waitForInitialization) {
+                    try {
+                        long startWaitMS = System.currentTimeMillis();
+                        while (!client.isInitialized()) {
+                            if (System.currentTimeMillis() - startWaitMS > 500) {
+                                System.out.println("Client initialization timed out after 500ms.");
+                                break;
+                            }
+                            Thread.sleep(50);
+                        }
+                    } catch (InterruptedException ie) {
+                        // no-op
+                    }
+                }
                 DataStore.LocalClients.put(body.clientId, client);
             }
 
@@ -96,6 +112,8 @@ public class ProxyController {
 
         Object[] parsedParams = this.parseParams(body);
 
+
+        logger.info("[COMMAND] " + body.command + (parsedParams.length > 0 ? ": " + Arrays.toString(Arrays.stream(parsedParams).toArray()) : ""));
         try {
             Object entity = getEntityFromLocation(request.getRequestURI());
 
@@ -114,7 +132,7 @@ public class ProxyController {
                     result.entityType.equals(EntityTypes.Client) ? new Object() : result.body
             );
         } catch (Exception e) {
-            logger.log(Level.INFO, e.toString());
+            logger.log(Level.INFO, "[COMMAND ERROR] " + body.command + ": " + e.toString());
             return body.isAsync ? new AsyncErrorResponse(e) : new ExceptionResponse(e);
         }
     }
