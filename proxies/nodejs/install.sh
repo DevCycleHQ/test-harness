1009605df58450e383c791fbb588284919366b69
Jonathan Norris <jonathan@taplytics.com>
2023-06-13 11:59:56 -0400

    update install.sh

--- proxies/nodejs/install.sh
+++ proxies/nodejs/install.sh
@@ -5,19 +5,17 @@ if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
     cd js-sdks
     git checkout $SDK_GITHUB_SHA
     yarn
-    NX_DAEMON=false yarn nx build openfeature-nodejs-provider --verbose
+    NX_DAEMON=false yarn nx build nodejs --verbose
     cd ..
 
     # convince yarn that these packages arent part of a workspace by writing empty lock files
     touch js-sdks/dist/sdk/nodejs/yarn.lock
     touch js-sdks/dist/lib/shared/types/yarn.lock
-    touch js-sdks/dist/lib/shared/bucketing-assembly-script/yarn.lock
-    touch js-sdks/dist/sdk/openfeature-nodejs-provider/yarn.lock
+    touch js-sdks/lib/shared/bucketing-assembly-script/yarn.lock
 
     yarn link js-sdks/dist/sdk/nodejs/
     yarn link js-sdks/lib/shared/bucketing-assembly-script/
     yarn link js-sdks/dist/lib/shared/types/
-    yarn link js-sdks/dist/sdk/openfeature-nodejs-provider/
 elif [ -z "$NODEJS_SDK_VERSION" ]; then
     yarn add @devcycle/nodejs-server-sdk@latest
 else
