839db01fce1b4a021f9da4715155457273d7f011
Jonathan Norris <jonathan@taplytics.com>
2023-06-14 11:57:49 -0400

    fix: nodejs install.sh script, add openfeature provider (#256)
    * fix: nodejs install.sh script, add openfeature provider
    
    * add yarn remove
    
    * update install.sh
    
    * update openfeature provider install.sh
    
    * print yarn why for packages
    
    * cleanup openfeature install script
    
    * update install scripts
    
    * cleanup
    
    * update SDKs to latest

--- proxies/nodejs/install.sh
+++ proxies/nodejs/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
     git clone https://github.com/DevCycleHQ/js-sdks.git
     cd js-sdks
@@ -7,15 +9,23 @@ if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
     cd ..
 
     # convince yarn that these packages arent part of a workspace by writing empty lock files
-    touch js-sdks/dist/sdk/nodejs/yarn.lock
+    echo "touch yarn.lock files"
     touch js-sdks/dist/lib/shared/types/yarn.lock
-    touch js-sdks/dist/lib/shared/bucketing-assembly-script/yarn.lock
+    touch js-sdks/lib/shared/bucketing-assembly-script/yarn.lock
+    touch js-sdks/dist/sdk/nodejs/yarn.lock
 
-    yarn link js-sdks/dist/sdk/nodejs/
-    yarn link js-sdks/lib/shared/bucketing-assembly-script/
     yarn link js-sdks/dist/lib/shared/types/
+    yarn link js-sdks/lib/shared/bucketing-assembly-script/
+    yarn link js-sdks/dist/sdk/nodejs/
+
+    echo "Installed nodejs-server-sdk@$SDK_GITHUB_SHA"
 elif [ -z "$NODEJS_SDK_VERSION" ]; then
     yarn add @devcycle/nodejs-server-sdk@latest
+    echo "Installed nodejs-server-sdk@latest"
 else
     yarn add @devcycle/nodejs-server-sdk@$NODEJS_SDK_VERSION
+    echo "Installed nodejs-server-sdk@$NODEJS_SDK_VERSION"
 fi
+
+echo "yarn nodejs-server-sdk why:"
+yarn why @devcycle/nodejs-server-sdk
--- proxies/openfeature-nodejs/install.sh
+++ proxies/openfeature-nodejs/install.sh
@@ -1,23 +1,36 @@
+#!/bin/bash
+
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"of-nodejs"* ]]; then
     git clone https://github.com/DevCycleHQ/js-sdks.git
     cd js-sdks
     git checkout $SDK_GITHUB_SHA
     yarn
-    NX_DAEMON=false yarn nx build nodejs --verbose
+    NX_DAEMON=false yarn nx build openfeature-nodejs-provider --verbose
     cd ..
 
+    echo "touch yarn.lock files"
     # convince yarn that these packages arent part of a workspace by writing empty lock files
     touch js-sdks/dist/sdk/nodejs/yarn.lock
-    touch js-sdks/dist/sdk/openfeature-nodejs/yarn.lock
+    touch js-sdks/dist/sdk/openfeature-nodejs-provider/yarn.lock
     touch js-sdks/dist/lib/shared/types/yarn.lock
-    touch js-sdks/dist/lib/shared/bucketing-assembly-script/yarn.lock
+    touch js-sdks/lib/shared/bucketing-assembly-script/yarn.lock
 
-    yarn link js-sdks/dist/sdk/nodejs/
-    yarn link js-sdks/dist/sdk/openfeature-nodejs/
-    yarn link js-sdks/lib/shared/bucketing-assembly-script/
+    echo "yarn link"
     yarn link js-sdks/dist/lib/shared/types/
-elif [ -z "OF_$NODEJS_SDK_VERSION" ]; then
-    yarn add @devcycle/nodejs-server-sdk@latest
+    yarn link js-sdks/lib/shared/bucketing-assembly-script/
+    yarn link js-sdks/dist/sdk/nodejs/
+    yarn link js-sdks/dist/sdk/openfeature-nodejs-provider/
+
+    echo "Installed openfeature-nodejs-provider@$SDK_GITHUB_SHA"
+elif [ -z "$OF_NODEJS_SDK_VERSION" ]; then
+    yarn add @devcycle/openfeature-nodejs-provider@latest
+    echo "Installed openfeature-nodejs-provider@latest"
 else
-    yarn add @devcycle/nodejs-server-sdk@$OF_NODEJS_SDK_VERSION
+    yarn add @devcycle/openfeature-nodejs-provider@$OF_NODEJS_SDK_VERSION
+    echo "Installed openfeature-nodejs-provider@$OF_NODEJS_SDK_VERSION"
 fi
+
+echo "yarn nodejs-server-sdk why:"
+yarn why @devcycle/nodejs-server-sdk
+echo "yarn openfeature-nodejs-provider why:"
+yarn why @devcycle/openfeature-nodejs-provider
