cc188dcb6cfb801c4a7cdd564da78286fc29a083
Jonathan Norris <jonathan@taplytics.com>
2023-06-14 10:37:26 -0400

    cleanup openfeature install script

--- proxies/openfeature-nodejs/install.sh
+++ proxies/openfeature-nodejs/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"of-nodejs"* ]]; then
-    #yarn remove @devcycle/openfeature-nodejs-provider
-    #yarn remove @devcycle/nodejs-server-sdk
-
     git clone https://github.com/DevCycleHQ/js-sdks.git
     cd js-sdks
     git checkout $SDK_GITHUB_SHA
@@ -11,25 +8,28 @@ if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"of-nodejs"* ]]; then
     NX_DAEMON=false yarn nx build openfeature-nodejs-provider --verbose
     cd ..
 
+    echo "touch yarn.lock files"
     # convince yarn that these packages arent part of a workspace by writing empty lock files
     touch js-sdks/dist/sdk/nodejs/yarn.lock
     touch js-sdks/dist/sdk/openfeature-nodejs-provider/yarn.lock
     touch js-sdks/dist/lib/shared/types/yarn.lock
     touch js-sdks/lib/shared/bucketing-assembly-script/yarn.lock
 
+    echo "yarn link"
+    yarn link js-sdks/dist/lib/shared/types/
+    yarn link js-sdks/lib/shared/bucketing-assembly-script/
     yarn link js-sdks/dist/sdk/nodejs/
     yarn link js-sdks/dist/sdk/openfeature-nodejs-provider/
-    yarn link js-sdks/lib/shared/bucketing-assembly-script/
-    yarn link js-sdks/dist/lib/shared/types/
 
-    echo "Installing openfeature-nodejs-provider@$SDK_GITHUB_SHA"
+    yarn
+    echo "Installed openfeature-nodejs-provider@$SDK_GITHUB_SHA"
 elif [ -z "$OF_NODEJS_SDK_VERSION" ]; then
-    echo "Installing latest version of openfeature-nodejs-provider"
     yarn add @devcycle/openfeature-nodejs-provider@latest
+    echo "Installed latest version of openfeature-nodejs-provider"
 else
-    echo "Installing openfeature-nodejs-provider@$OF_NODEJS_SDK_VERSION"
     yarn add @devcycle/openfeature-nodejs-provider@$OF_NODEJS_SDK_VERSION
+    echo "Installed openfeature-nodejs-provider@$OF_NODEJS_SDK_VERSION"
 fi
 
-echo "yarn nodejs-server-sdk why: $(yarn why @devcycle/nodejs-server-sdk)"
-echo "yarn openfeature-nodejs-provider why: $(yarn why @devcycle/openfeature-nodejs-provider)"
+echo "yarn nodejs-server-sdk why: /n$(yarn why @devcycle/nodejs-server-sdk)"
+echo "yarn openfeature-nodejs-provider why: /n$(yarn why @devcycle/openfeature-nodejs-provider)"
