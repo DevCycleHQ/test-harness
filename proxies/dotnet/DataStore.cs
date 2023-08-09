59b2077630fd22b6622562e7b6a7459942aa7611
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-08-09 16:00:11 -0700

    Update proxy to match sdk api changes

--- proxies/dotnet/DataStore.cs
+++ proxies/dotnet/DataStore.cs
@@ -1,18 +1,17 @@
 using DevCycle.SDK.Server.Cloud.Api;
 using DevCycle.SDK.Server.Local.Api;
-using DevCycle.SDK.Server.Common.Model;
 
 class DataStore
 {
-    public static Dictionary<string, DVCLocalClient> LocalClients;
-    public static Dictionary<string, DVCCloudClient> CloudClients;
+    public static Dictionary<string, DevCycleLocalClient> LocalClients;
+    public static Dictionary<string, DevCycleCloudClient> CloudClients;
 
     public static Dictionary<string, object> Commands;
 
     static DataStore()
     {
-        CloudClients = new Dictionary<string, DVCCloudClient>();
-        LocalClients = new Dictionary<string, DVCLocalClient>();
+        CloudClients = new Dictionary<string, DevCycleCloudClient>();
+        LocalClients = new Dictionary<string, DevCycleLocalClient>();
         Commands = new Dictionary<string, object>();
     }
 }
