e803ce6caa5b8bda0659b2a0834e1eeb11328919
Jamie Sinn <jamie.sinn@taplytics.com>
2024-02-06 14:29:16 -0500

    Update lock file?

--- proxies/php/php-proxy/install.sh
+++ proxies/php/php-proxy/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
 
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"php"* ]]; then
-  echo "$SDK_GITHUB_SHA"
+  echo "Installing PHP SDK at $SDK_GITHUB_SHA"
   composer require "devcycle/php-server-sdk:dev-main#$SDK_GITHUB_SHA"
 elif [ -n "$PHP_SDK_VERSION" ]; then
-    composer require "devcycle/php-server-sdk:$PHP_SDK_VERSION"
+  echo "Installing PHP SDK Version ${PHP_SDK_VERSION}"
+  composer require "devcycle/php-server-sdk:$PHP_SDK_VERSION"
 else
-    composer require "devcycle/php-server-sdk:dev-main"
+  echo "Installing latest PHP SDK"
+  composer require "devcycle/php-server-sdk:dev-main"
 fi
