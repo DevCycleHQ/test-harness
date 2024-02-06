88232a65d533c24dd79fcea696d9b6462b5c145d
Jamie Sinn <jamie.sinn@taplytics.com>
2024-02-06 10:10:48 -0500

    Fix install script

--- proxies/php/php-proxy/install.sh
+++ proxies/php/php-proxy/install.sh
@@ -3,8 +3,8 @@
 if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"php"* ]]; then
   echo "$SDK_GITHUB_SHA"
   composer require "devcycle/php-server-sdk:dev-main#$SDK_GITHUB_SHA"
-elif [ -z "$PHP_SDK_VERSION" ]; then
-  composer require "devcycle/php-server-sdk:dev-main"
+elif [ -n "$PHP_SDK_VERSION" ]; then
+    composer require "devcycle/php-server-sdk:$PHP_SDK_VERSION"
 else
-  composer require "devcycle/php-server-sdk:$PHP_SDK_VERSION"
+    composer require "devcycle/php-server-sdk:dev-main"
 fi
