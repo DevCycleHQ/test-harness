055f0551e87cfac145d9be3e5d73c4b33fad69a5
Adam Wootton <adam@taplytics.com>
2024-05-02 15:12:09 -0400

    add bootstrapping tests

---
+++ harness/helpers/commands.ts
@@ -0,0 +1,139 @@
+export type CommandBody = {
+    command: string
+    isAsync?: boolean
+    params: ({ value: unknown } | { type: 'user' | 'event' })[]
+    user?: Record<string, unknown>
+    event?: Record<string, unknown>
+}
+export const sendCommand = async (url: string, body: CommandBody) => {
+    return await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            command: body.command,
+            isAsync: body.isAsync,
+            params: body.params,
+            user: body.user,
+            event: body.event,
+        }),
+    })
+}
+export const callVariableValue = async (
+    url: string,
+    user: Record<string, unknown>,
+    sdkName: string,
+    isAsync: boolean,
+    key?: string,
+    variableType?: string,
+    defaultValue?: any,
+) => {
+    return await performCallVariable(
+        url,
+        user,
+        sdkName,
+        isAsync,
+        key,
+        variableType,
+        defaultValue,
+        'variableValue',
+    )
+}
+export const callVariable = async (
+    url: string,
+    user: Record<string, unknown>,
+    sdkName: string,
+    isAsync: boolean,
+    key?: string,
+    variableType?: string,
+    defaultValue?: any,
+) => {
+    return await performCallVariable(
+        url,
+        user,
+        sdkName,
+        isAsync,
+        key,
+        variableType,
+        defaultValue,
+        'variable',
+    )
+}
+export const performCallVariable = async (
+    url: string,
+    user: Record<string, unknown>,
+    sdkName: string,
+    isAsync: boolean,
+    key?: string,
+    variableType?: string,
+    defaultValue?: any,
+    command = 'variable',
+) => {
+    const params: CommandBody['params'] = [
+        { type: 'user' },
+        { value: key },
+        { value: defaultValue },
+    ]
+    // Need to pass in the variable type into the OpenFeature provider as it doesn't have a generic variable interface
+    if (sdkName === 'OF-NodeJS') {
+        params.push({ value: variableType })
+    }
+    return await sendCommand(url, {
+        command,
+        user,
+        params,
+        isAsync,
+    })
+}
+export const callAllVariables = async (
+    url: string,
+    user: Record<string, unknown>,
+    isAsync: boolean,
+) => {
+    return await sendCommand(url, {
+        command: 'allVariables',
+        user,
+        params: [{ type: 'user' }],
+        isAsync,
+    })
+}
+export const callTrack = async (
+    url: string,
+    user: Record<string, unknown>,
+    event: Record<string, unknown>,
+    isAsync: boolean,
+) => {
+    return await sendCommand(url, {
+        command: 'track',
+        user,
+        event,
+        params: [{ type: 'user' }, { type: 'event' }],
+        isAsync,
+    })
+}
+export const callAllFeatures = async (
+    url: string,
+    user: Record<string, unknown>,
+    isAsync: boolean,
+) => {
+    return await sendCommand(url, {
+        command: 'allFeatures',
+        user,
+        params: [{ type: 'user' }],
+        isAsync,
+    })
+}
+
+export const callGetClientBootstrapConfig = async (
+    url: string,
+    user: Record<string, unknown>,
+    userAgent: string,
+) => {
+    return await sendCommand(url, {
+        command: 'getClientBootstrapConfig',
+        user,
+        params: [{ type: 'user' }, { value: userAgent }],
+        isAsync: true,
+    })
+}
--- proxies/nodejs/install.sh
+++ proxies/nodejs/install.sh
@@ -13,6 +13,7 @@ if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
     # TODO find out why this is necessary
     yarn
     NX_DAEMON=false yarn nx build nodejs --verbose
+    NX_DAEMON=false yarn nx build js --verbose
     cd ..
 
     # convince yarn that these packages arent part of a workspace by writing empty lock files
@@ -20,24 +21,30 @@ if [ -n "$SDK_GITHUB_SHA" ] && [[ "$SDKS_TO_TEST" =~ .*"nodejs"* ]]; then
     touch js-sdks/dist/lib/shared/types/yarn.lock
     touch js-sdks/lib/shared/bucketing-assembly-script/yarn.lock
     touch js-sdks/dist/sdk/nodejs/yarn.lock
+    touch js-sdks/dist/sdk/js/yarn.lock
 
     echo "yarn link, yarn version: $(yarn -v)"
     yarn link js-sdks/dist/lib/shared/types/
     yarn link js-sdks/lib/shared/bucketing-assembly-script/
     yarn link js-sdks/dist/sdk/nodejs/
+    yarn link js-sdks/dist/sdk/js/
 
     echo "Installed nodejs-server-sdk@$SDK_GITHUB_SHA"
 elif [ -z "$NODEJS_SDK_VERSION" ]; then
     echo "Installing nodejs-server-sdk@latest"
 
     yarn add @devcycle/nodejs-server-sdk@latest
+    yarn add @devcycle/js-client-sdk@latest
     echo "Installed nodejs-server-sdk@latest"
 else
     echo "Installing nodejs-server-sdk@$NODEJS_SDK_VERSION"
 
     yarn add @devcycle/nodejs-server-sdk@$NODEJS_SDK_VERSION
+    yarn add @devcycle/js-client-sdk@latest
     echo "Installed nodejs-server-sdk@$NODEJS_SDK_VERSION"
 fi
 
 echo "yarn nodejs-server-sdk why:"
 yarn why @devcycle/nodejs-server-sdk
+echo "yarn js-client-sdk why:"
+yarn why @devcycle/js-client-sdk
