011964ffb681deb14ab48fa44c35e18cdfa4d6ef
Jonathan Norris <jonathan@taplytics.com>
2025-02-11 10:49:25 -0500

    feat: add sdk-capabilities input to github action

--- harness/types/capabilities.ts
+++ harness/types/capabilities.ts
@@ -38,7 +38,8 @@ let sdkCapabilities: { [key: string]: string[] } = {
         Capabilities.sdkConfigEvent,
         Capabilities.clientUUID,
         Capabilities.v2Config,
-        Capabilities.sdkPlatform,
+        // Comment out until changes are merged, will use SDK_CAPABILITIES to override
+        // Capabilities.sdkPlatform,
     ],
     Python: [
         Capabilities.cloud,
