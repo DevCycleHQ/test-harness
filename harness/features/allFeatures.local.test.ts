7e8b6df1d81deae1833a32c63f1f323234f3fe64
phaym <1431593+phaym@users.noreply.github.com>
2025-07-16 11:03:19 -0400

    format with indentation

--- harness/features/allFeatures.local.test.ts
+++ harness/features/allFeatures.local.test.ts
@@ -1,4 +1,9 @@
-import { LocalTestClient, getSDKScope, hasCapability, describeCapability } from '../helpers'
+import {
+    LocalTestClient,
+    getSDKScope,
+    hasCapability,
+    describeCapability,
+} from '../helpers'
 import { Capabilities } from '../types'
 import { expectedFeaturesVariationOn } from '../mockData'
 
@@ -6,65 +11,66 @@ describe('allFeatures Tests - Local', () => {
     const { sdkName, scope } = getSDKScope()
 
     describeCapability(sdkName, Capabilities.allFeatures)(sdkName, () => {
+        describe('uninitialized client', () => {
+            let testClient: LocalTestClient
 
-    describe('uninitialized client', () => {
-        let testClient: LocalTestClient
+            beforeEach(async () => {
+                testClient = new LocalTestClient(sdkName)
+                const configRequestUrl = testClient.getValidConfigPath()
+                scope.get(configRequestUrl).times(2).reply(500)
+                await testClient.createClient(true, {
+                    configPollingIntervalMS: 60000,
+                })
+            })
 
-        beforeEach(async () => {
-            testClient = new LocalTestClient(sdkName)
-            const configRequestUrl = testClient.getValidConfigPath()
-            scope.get(configRequestUrl).times(2).reply(500)
-            await testClient.createClient(true, {
-                configPollingIntervalMS: 60000,
+            it('should return empty object if client is uninitialized', async () => {
+                const featuresResponse = await testClient.callAllFeatures({
+                    user_id: 'user1',
+                    customData: { 'should-bucket': true },
+                })
+                const features = await featuresResponse.json()
+                expect(features).toMatchObject({})
             })
         })
 
-        it('should return empty object if client is uninitialized', async () => {
-            const featuresResponse = await testClient.callAllFeatures({
-                user_id: 'user1',
-                customData: { 'should-bucket': true },
+        describe('initialized client', () => {
+            let testClient: LocalTestClient
+
+            beforeEach(async () => {
+                testClient = new LocalTestClient(sdkName, scope)
+                if (hasCapability(sdkName, Capabilities.sdkConfigEvent)) {
+                    scope
+                        .post(`/client/${testClient.clientId}/v1/events/batch`)
+                        .reply(201, {
+                            message: 'Successfully received events.',
+                        })
+                }
+
+                await testClient.createClient(true, {
+                    configPollingIntervalMS: 60000,
+                })
             })
-            const features = await featuresResponse.json()
-            expect(features).toMatchObject({})
+
+            it('should return all features for user without custom data', async () => {
+                const featuresResponse = await testClient.callAllFeatures({
+                    user_id: 'user3',
+                })
+                const features = (await featuresResponse.json()).data
+                expect(features).toMatchObject({
+                    'schedule-feature': {
+                        ...expectedFeaturesVariationOn['schedule-feature'],
+                    },
+                })
+            })
+
+            it('should return all features for user with custom data', async () => {
+                const featuresResponse = await testClient.callAllFeatures({
+                    user_id: 'user1',
+                    customData: { 'should-bucket': true },
+                })
+                const features = (await featuresResponse.json()).data
+                expect(features).toMatchObject(expectedFeaturesVariationOn)
+            })
         })
-    })
-
-    describe('initialized client', () => {
-        let testClient: LocalTestClient
-
-        beforeEach(async () => {
-            testClient = new LocalTestClient(sdkName, scope)
-            if (hasCapability(sdkName, Capabilities.sdkConfigEvent)) {
-                scope
-                    .post(`/client/${testClient.clientId}/v1/events/batch`)
-                    .reply(201, { message: 'Successfully received events.' })
-            }
-
-            await testClient.createClient(true, {
-                configPollingIntervalMS: 60000,
-            })
-        })
-
-        it('should return all features for user without custom data', async () => {
-            const featuresResponse = await testClient.callAllFeatures({
-                user_id: 'user3',
-            })
-            const features = (await featuresResponse.json()).data
-            expect(features).toMatchObject({
-                'schedule-feature': {
-                    ...expectedFeaturesVariationOn['schedule-feature'],
-                },
-            })
-        })
-
-        it('should return all features for user with custom data', async () => {
-            const featuresResponse = await testClient.callAllFeatures({
-                user_id: 'user1',
-                customData: { 'should-bucket': true },
-            })
-            const features = (await featuresResponse.json()).data
-            expect(features).toMatchObject(expectedFeaturesVariationOn)
-        })
-    })
     })
 })
